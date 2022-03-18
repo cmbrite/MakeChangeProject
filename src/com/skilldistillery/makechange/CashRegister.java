ea15b949d0a7a4f7b5b3dc2594e3305201d7dc73
Chris Brite <chrismbrite@gmail.com>
2022-03-18 16:38:09 -0700

    Add user stories to main

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -6,7 +6,17 @@ public class CashRegister {
 
 	public static void main(String[] args) {
 		Scanner cashIn = new Scanner(System.in);
-
+//		User Story #1
+//		The user is prompted asking for the price of the item.
+//
+//		User Story #2
+//		The user is then prompted asking how much money was tendered by the customer.
+//
+//		User Story #3
+//		Display an appropriate message if the customer provided too little money or the exact amount.
+//
+//		User Story #4
+//		If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer.
 		System.out.println("Hello! How much does your item cost? ");
 		double cost = cashIn.nextDouble();
 		System.out.println("How much money was tendered? ");
@@ -15,16 +25,12 @@ public class CashRegister {
 		String quarter = "quarter";
 		String dime = "dime";
 		String nickel = "nickel";
-		
 
-		
 		if (cashTendered < cost) {
 			System.out.println("You haven't paid enough for the item!");
-		} 
-		else if (cashTendered == cost) {
+		} else if (cashTendered == cost) {
 			System.out.println("Thanks for your business. Have a nice day!");
-		}
-		else if  (cashTendered > cost) {
+		} else if (cashTendered > cost) {
 			System.out.print("You change is " + change);
 		}
 
