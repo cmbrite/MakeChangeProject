f93ba90a836afc5939b26d8d5dedab505c812700
Chris Brite <chrismbrite@gmail.com>
2022-03-18 16:32:08 -0700

    Stub the README

---
+++ README.md
@@ -0,0 +1,18 @@
+## Make Change Project
+
+### Week 1 Homework for Skill Distillery
+
+## Overview
+
+Cash register simulator.
+TODO: FINISH WRITING THIS!!!
+
+## Technologies/Skills used
+
+* Java
+* Eclipse
+* TODO: MORE
+
+## Lessons Learned
+
+TODO: WRITE THIS
--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -12,7 +12,12 @@ public class CashRegister {
 		System.out.println("How much money was tendered? ");
 		double cashTendered = cashIn.nextDouble();
 		double change = cashTendered - cost;
+		String quarter = "quarter";
+		String dime = "dime";
+		String nickel = "nickel";
+		
 
+		
 		if (cashTendered < cost) {
 			System.out.println("You haven't paid enough for the item!");
 		} 
@@ -20,7 +25,7 @@ public class CashRegister {
 			System.out.println("Thanks for your business. Have a nice day!");
 		}
 		else if  (cashTendered > cost) {
-			System.out.println("You change is " + change);
+			System.out.print("You change is " + change);
 		}
 
 	}
