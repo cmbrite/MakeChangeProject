529ad4b8eca3aa15c41e4230cd41851f54cd28e1
Chris Brite <chrismbrite@gmail.com>
2022-03-21 01:48:19 -0700

    Updated Dollar variables

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -6,7 +6,7 @@ public class CashRegister {
 
 	public static void main(String[] args) {
 		Scanner cashIn = new Scanner(System.in);
-		
+
 //		The user is prompted asking for the price of the item.
 		System.out.println("Hello! How much does your item cost? ");
 		double cost = cashIn.nextDouble();
@@ -25,38 +25,34 @@ public class CashRegister {
 //
 //		If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer.
 		double change = cashTendered - cost;
-		int tenDollarBills = (int) change / 10; //TODO Get ten dollar bill total to show up without minimal if() functions
+		int tenDollarBills = (int) change / 10; // TODO Get ten dollar bill total to show up without minimal if()
+												// functions
 		int fiveDollarBills = (int) change % 10;
-		int oneDollarBills = (int)change % 5;
-		
+		int oneDollarBills = (int) change % 5;
+
 //		Dollar calculations are working accurately!
 		if (cashTendered > cost) {
 			System.out.print("Your change is ");
 		}
-		if (change >= 15) {
-			System.out.print(tenDollarBills + " ten dollar bill, " + "1 five dollar bill, "+ oneDollarBills + " one dollar bills");
+		if (change >= 10) {
+			System.out.print(tenDollarBills + " ten dollar bill,");
+		} if (change % 10 >=5  || change >= 5 && change < 10) {
+			System.out.print(" 1 five dollar bill, ");
+		} if (change >= 1) {
+			System.out.print(oneDollarBills + " one dollar bills ");
 		}
-		else if (change >= 10) {
-			System.out.print("1 ten dollar bill, " + oneDollarBills + " one dollar bills");
-		}
-		else if (change >= 5) {
-			System.out.print("1 five dollar bill, " + oneDollarBills + " one dollar bills");
-		} 
-		else if (change >= 1) {
-			System.out.print(oneDollarBills + " one dollar bills");
-		} 
 
 //		Noted out coins until dollars are fully working. May change format.
-		String quarter = "quarter";
-//		String dime = "dime";
-//		String nickel = "nickel";
-//		String penny = "penny";
-//		double quarter1 = 0.25;
-//		double dime1 = 0.10;
-//		double nickel1 = 0.5;
-//		double penny1 = 0.01;
 		double coins = change - ((int)change);
-	
-		System.out.println(coins); //This print is to test coins
+		double quarter1 = coins / 0.25;
+		double dime1 = 0.10;
+		double nickel1 = 0.5;
+		double penny1 = 0.01;
+		
+		if (quarter1 > 0)
+			System.out.print((int)quarter1 + " quarters");
 
+//		System.out.println(coins); // This print is to test coins
+
+	}
 }
