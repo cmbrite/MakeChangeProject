a56a30b14c9f9dcb1eee887b7719ab8f46082c9d
Chris Brite <chrismbrite@gmail.com>
2022-03-21 01:13:58 -0700

    Testing Coins

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -6,6 +6,7 @@ public class CashRegister {
 
 	public static void main(String[] args) {
 		Scanner cashIn = new Scanner(System.in);
+		
 //		The user is prompted asking for the price of the item.
 		System.out.println("Hello! How much does your item cost? ");
 		double cost = cashIn.nextDouble();
@@ -28,6 +29,7 @@ public class CashRegister {
 		int fiveDollarBills = (int) change % 10;
 		int oneDollarBills = (int)change % 5;
 		
+//		Dollar calculations are working accurately!
 		if (cashTendered > cost) {
 			System.out.print("Your change is ");
 		}
@@ -53,7 +55,8 @@ public class CashRegister {
 //		double dime1 = 0.10;
 //		double nickel1 = 0.5;
 //		double penny1 = 0.01;
-		double coins;
-	}
+		double coins = change - ((int)change);
+	
+		System.out.println(coins); //This print is to test coins
 
 }
